c7375fe1e161c66106cde5ef97eb6f648526e8d6
mufis-coder <muhamadfikrisunandar@gmail.com>
2022-05-22 19:13:47 +0700

    feat: add draw_window function in main py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import sys
+import pygame
+import os
 
 sys.path.append('/object_game/')
 
 from object_game.pipe import Pipe as pipe
+
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
+
+def draw_window(win, birds, pipes, base, score):
+    win.blit(BG_IMG, (0, 0))
+
+    for pipe in pipes:
+        pipe.draw(win)
+    
