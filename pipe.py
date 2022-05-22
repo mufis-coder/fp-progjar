541f3f739d4bba89053d032e7ca68ffb8febdae5
mufis-coder <muhamadfikrisunandar@gmail.com>
2022-05-22 13:48:49 +0700

    feat: add img src for pipe

--- pipe.py
+++ pipe.py
@@ -1,4 +1,8 @@
 import random
+import pygame
+import os
+
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
 
 class Pipe:
     GAP = 200
@@ -11,13 +15,19 @@ class Pipe:
 
         self.top = 0
         self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTTOM = PIPE_IMG
 
         self.passed = False
         self.set_height()
     
     def set_height(self):
         self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
         self.bottom = self.height + self.GAP
     
     def move(self):
         self.x -= self.VEL
+    
+    def draw(self, win):
+        pass
